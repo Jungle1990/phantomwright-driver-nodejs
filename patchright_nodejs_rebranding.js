609c383cc5b99b042b10bb96d32294b61815aed6
Wei ZHANG <zhangwei@microsoft.com>
2026-01-27 13:42:27 +0800

    update dep from patchright to phantomwright

--- patchright_nodejs_rebranding.js
+++ patchright_nodejs_rebranding.js
@@ -118,13 +118,13 @@ function renameImportsAndExportsInDirectory(directoryPath) {
         sourceFile.getImportDeclarations().forEach((importDecl) => {
             const moduleSpecifierValue = importDecl.getModuleSpecifierValue();
 
-            // If the import path starts with "playwright-core", replace it with "patchright-core"
+            // If the import path starts with "playwright-core", replace it with "phantomwright-core"
             if (moduleSpecifierValue.startsWith("playwright-core")) {
-                const newModuleSpecifier = moduleSpecifierValue.replace("playwright-core", "patchright-core");
+                const newModuleSpecifier = moduleSpecifierValue.replace("playwright-core", "phantomwright-core");
                 importDecl.setModuleSpecifier(newModuleSpecifier);
                 modified = true;
             } else if (moduleSpecifierValue.includes("playwright-core")) {
-                const newModuleSpecifier = moduleSpecifierValue.replace(/playwright-core/g, "patchright-core");
+                const newModuleSpecifier = moduleSpecifierValue.replace(/playwright-core/g, "phantomwright-core");
                 importDecl.setModuleSpecifier(newModuleSpecifier);
                 modified = true;
             }
@@ -134,9 +134,9 @@ function renameImportsAndExportsInDirectory(directoryPath) {
         sourceFile.getExportDeclarations().forEach((exportDecl) => {
             const moduleSpecifierValue = exportDecl.getModuleSpecifierValue();
 
-            // If the export path starts with "playwright-core", replace it with "patchright-core"
+            // If the export path starts with "playwright-core", replace it with "phantomwright-core"
             if (moduleSpecifierValue && moduleSpecifierValue.startsWith("playwright-core")) {
-                const newModuleSpecifier = moduleSpecifierValue.replace("playwright-core", "patchright-core");
+                const newModuleSpecifier = moduleSpecifierValue.replace("playwright-core", "phantomwright-core");
                 exportDecl.setModuleSpecifier(newModuleSpecifier);
                 modified = true;
             }
@@ -148,7 +148,7 @@ function renameImportsAndExportsInDirectory(directoryPath) {
         });
 
         exportAllDeclarations.forEach(exportDecl => {
-            exportDecl.setModuleSpecifier('patchright-core');
+            exportDecl.setModuleSpecifier('phantomwright-core');
             modified = true;
         });
 
@@ -165,11 +165,11 @@ function renameImportsAndExportsInDirectory(directoryPath) {
             const args = call.getArguments();
             if (args.length && (args[0].getText().includes("playwright-core"))) {
                 const arg = args[0];
-                arg.replaceWithText(arg.getText().replace(/playwright-core/g, "patchright-core"));
+                arg.replaceWithText(arg.getText().replace(/playwright-core/g, "phantomwright-core"));
                 modified = true;
             } else if (args.length && (args[0].getText().includes("playwright"))) {
                 const arg = args[0];
-                arg.replaceWithText(arg.getText().replace(/playwright/g, "patchright"));
+                arg.replaceWithText(arg.getText().replace(/playwright/g, "phantomwright"));
                 modified = true;
             }
         });
@@ -184,32 +184,32 @@ function renameImportsAndExportsInDirectory(directoryPath) {
 
 
 // Renaming the folders and using context managers to ensure they finished
-fs.rename("packages/playwright-core", "packages/patchright-core", (err) => {
-    fs.rename("packages/playwright", "packages/patchright", (err) => {
+fs.rename("packages/playwright-core", "packages/phantomwright-core", (err) => {
+    fs.rename("packages/playwright", "packages/phantomwright", (err) => {
         // Write the Projects README to the README which is used in the release
         fs.readFile("../README.md", "utf8", (err, data) => {
-            fs.writeFileSync("packages/patchright/README.md", data, "utf8", (err) => {});
+            fs.writeFileSync("packages/phantomwright/README.md", data, "utf8", (err) => {});
         });
-        fs.writeFileSync("packages/patchright-core/README.md", "# patchright-core\n\nThis package contains the no-browser flavor of [Patchright-NodeJS](https://github.com/Kaliiiiiiiiii-Vinyzu/patchright-nodejs).", "utf8", (err) => {});
+        fs.writeFileSync("packages/phantomwright-core/README.md", "# phantomwright-core\n\nThis package contains the no-browser flavor of [Phantomwright-NodeJS](https://github.com/Jungle1990/phantomwright-driver-nodejs).", "utf8", (err) => {});
 
         // Package.Json Files
         // playwright-core/package.json
-        fs.readFile("packages/patchright-core/package.json", "utf8", (err, data) => {
+        fs.readFile("packages/phantomwright-core/package.json", "utf8", (err, data) => {
           const packageJson = JSON.parse(data);
-          packageJson.name = "patchright-core";
+          packageJson.name = "phantomwright-core";
           if (process.env.patchright_release && process.env.patchright_release.trim() !== "") {
             packageJson.version = process.env.patchright_release;
           }
 
-          packageJson.author["name"] = "Microsoft Corportation, patched by github.com/Kaliiiiiiiiii-Vinyzu/";
-          packageJson.homepage = "https://github.com/Kaliiiiiiiiii-Vinyzu/patchright-nodejs"
-          packageJson.repository["url"] = "https://github.com/Kaliiiiiiiiii-Vinyzu/patchright-nodejs"
+          packageJson.author["name"] = "Microsoft Corportation, patched by github.com/Jungle1990/";
+          packageJson.homepage = "https://github.com/Jungle1990/phantomwright-driver-nodejs"
+          packageJson.repository["url"] = "https://github.com/Jungle1990/phantomwright-driver-nodejs"
           packageJson.bin = {
-            "patchright-core": "cli.js"
+            "phantomwright-core": "cli.js"
           }
 
           const updatedJsonData = JSON.stringify(packageJson, null, 4);
-          fs.writeFile("packages/patchright-core/package.json", updatedJsonData, 'utf8', (err) => {
+          fs.writeFile("packages/phantomwright-core/package.json", updatedJsonData, 'utf8', (err) => {
             if (err) {
                         console.log('Error writing to the file:', err);
                     } else {
@@ -218,24 +218,24 @@ fs.rename("packages/playwright-core", "packages/patchright-core", (err) => {
           });
         });
         // playwright/package.json
-        fs.readFile("packages/patchright/package.json", "utf8", (err, data) => {
+        fs.readFile("packages/phantomwright/package.json", "utf8", (err, data) => {
           const packageJson = JSON.parse(data);
-          packageJson.name = "patchright";
+          packageJson.name = "phantomwright";
           if (process.env.patchright_release && process.env.patchright_release.trim() !== "") {
             packageJson.version = process.env.patchright_release;
           }
-          packageJson.author["name"] = "Microsoft Corportation, patched by github.com/Kaliiiiiiiiii-Vinyzu/";
-          packageJson.homepage = "https://github.com/Kaliiiiiiiiii-Vinyzu/patchright-nodejs"
-          packageJson.repository["url"] = "https://github.com/Kaliiiiiiiiii-Vinyzu/patchright-nodejs"
+          packageJson.author["name"] = "Microsoft Corportation, patched by github.com/Jungle1990/";
+          packageJson.homepage = "https://github.com/Jungle1990/phantomwright-driver-nodejs"
+          packageJson.repository["url"] = "https://github.com/Jungle1990/phantomwright-driver-nodejs"
           packageJson.bin = {
-            "patchright": "cli.js"
+            "phantomwright": "cli.js"
           }
           packageJson.dependencies = {
-            "patchright-core": packageJson.version
+            "phantomwright-core": packageJson.version
           }
 
           const updatedJsonData = JSON.stringify(packageJson, null, 4);
-          fs.writeFile("packages/patchright/package.json", updatedJsonData, 'utf8', (err) => {
+          fs.writeFile("packages/phantomwright/package.json", updatedJsonData, 'utf8', (err) => {
             if (err) {
                         console.log('Error writing to the file:', err);
                     } else {
@@ -245,6 +245,6 @@ fs.rename("packages/playwright-core", "packages/patchright-core", (err) => {
         });
 
         // Usage example: pass the directory path as an argument
-        renameImportsAndExportsInDirectory("packages/patchright");
+        renameImportsAndExportsInDirectory("packages/phantomwright");
     })
 })
